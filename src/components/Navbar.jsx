fdfac238981e713b3851c4dbe5fbc6cc427f0da4
akrmSujan <sujanban223@gmail.com>
2025-06-28 11:54:54 +0600

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,48 @@
 import React, { use } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { Menu, Moon, Sun } from 'lucide-react';
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
-    const {theme,toggleTheme}=use(AuthContext)
+    const { theme, toggleTheme } = use(AuthContext)
+    const navItems = <>
+        <li><NavLink>Home</NavLink></li>
+        <li><NavLink>About me</NavLink></li>
+        <li><NavLink>Projects</NavLink></li>
+        <li><NavLink>Contact</NavLink></li>
+    </>
     return (
-      <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-       <Menu/>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <label onClick={toggleTheme} className={`cursor-pointer fixed bg-blue-600 dark:bg-blue-900
+        <div className="navbar bg-base-100 px-6 lg:px-12 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <Menu />
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu text-[16px] min-w-screen dropdown-content bg-base-100 rounded-box z-1  shadow">
+                        {
+                            navItems
+                        }
+                    </ul>
+                </div>
+                <a className="text-2xl lg:text-4xl">Su Jan</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu text-[16px] font-bold menu-horizontal px-1">
+                    {
+                        navItems
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <label onClick={toggleTheme} className={`cursor-pointer fixed bg-blue-600 dark:bg-blue-900
  md:p-2 p-1 z-100 rounded-full swap swap-rotate ${theme === 'dark' ? 'swap-active' : ''}`}>
-          <Moon size={20} className="swap-on text-gray-400" />
-          <Sun size={20} className="swap-off text-yellow-500" />
-        </label>
-  </div>
-</div>
+                    <Moon size={20} className="swap-on text-gray-400" />
+                    <Sun size={20} className="swap-off text-yellow-500" />
+                </label>
+            </div>
+        </div>
     );
 };
 
