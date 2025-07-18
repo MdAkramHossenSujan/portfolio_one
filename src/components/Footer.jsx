a649c0b99d731ccfaa7c3635b599e4ec0d79b26e
akrmSujan <sujanban223@gmail.com>
2025-07-19 01:04:54 +0600

    Made responvive footer and added commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,13 @@ import dayjs from "dayjs";
 import {  IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 import logo from '../assets/Black White Simple Initials Logo.png'
 export default function Footer() {
+    //State to set current time
     const [currentTime, setCurrentTime] = useState(dayjs());
+    //UseEffect to scroll to top when pages changes
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
+    //UseEffect to set current time
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentTime(dayjs());
@@ -14,7 +17,7 @@ export default function Footer() {
 
         return () => clearInterval(interval);
     }, []);
-
+    //Return the footer component
     return (
         <footer className="w-full text-center py-4 px-2 bg-base-300 dark:bg-gray-950 text-gray-700 dark:text-gray-300 text-sm">
             <div className="flex justify-center text-3xl lg:text-4xl xl:text-5xl items-center ">
@@ -33,15 +36,17 @@ export default function Footer() {
                 <span>Mindset</span>
                 <span>Thinker</span>
             </div>
+            {/*Clicking anchor tag will take to specific div */}
             <div className="mb-2 flex flex-wrap justify-center gap-3 lg:gap-5 xl:gap-8">
                 <a href="#aboutme">About Me</a>
                 <a href="#projects">Projects</a>
             </div>
+            {/*Current time display*/}
             <div className="mb-2 flex flex-col sm:flex-row justify-center items-center gap-2">
                 <span className="font-semibold text-gray-600 dark:text-cyan-400">
                     {currentTime.format("HH:mm:ss")}
                 </span>
-                <span>|</span>
+                <span className="hidden md:block">|</span>
                 <span>{currentTime.format("ddd, MMM D, YYYY")}</span>
             </div>
             {/* Copyright */}
